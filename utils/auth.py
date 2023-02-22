0da3e1eac5b7451141ee63a404351e778952f530
carban14 <>
2023-02-23 00:06:53 +0800

    no limit on user if ALLOWED_USERS not set

--- utils/auth.py
+++ utils/auth.py
@@ -13,12 +13,15 @@ def auth():
     def decorator(func: callable):
         @wraps(func)
         async def wrapper(update, context):
-            if update.effective_user.username in allowed_users:
+            if len(allowed_users) == 0:
                 await func(update, context)
             else:
-                await update.message.reply_text(
-                    "You are not authorized to use this bot"
-                )
+                if update.effective_user.id in allowed_users:
+                    await func(update, context)
+                else:
+                    await update.message.reply_text(
+                        "You are not authorized to use this bot"
+                    )
 
         return wrapper
 
